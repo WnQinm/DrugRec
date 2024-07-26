502ed50b3bf5c076ab861aaadc800b01fe25cfda
温清梦 <flidwala@gmail.com>
2024-07-26 16:53:22 +0800

    更改url处理逻辑；简化模型代码

--- src/model/contriver.py
+++ src/model/contriver.py
@@ -8,17 +8,16 @@ class ContrieverScorer:
     def __init__(self, tokenizer_path, retriever_ckpt_path, device=None, max_batch_size=400) -> None:
         query_encoder_path = os.path.join(retriever_ckpt_path, 'query_encoder')
         reference_encoder_path = os.path.join(retriever_ckpt_path, 'reference_encoder')
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") if not device else device
 
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        self.query_encoder = AutoModel.from_pretrained(query_encoder_path)
-        self.reference_encoder = AutoModel.from_pretrained(reference_encoder_path)
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") if not device else device
-        self.query_encoder = self.query_encoder.to(self.device).eval()
-        self.reference_encoder = self.reference_encoder.to(self.device).eval()
+        self.query_encoder = AutoModel.from_pretrained(query_encoder_path).to(self.device).eval()
+        self.reference_encoder = AutoModel.from_pretrained(reference_encoder_path).to(self.device).eval()
+
         assert max_batch_size > 0
         self.max_batch_size = max_batch_size
 
-    def get_query_embeddings(self, sentences: List[str]) -> torch.Tensor:
+    def get_embeddings(self, sentences: List[str], mode: str) -> torch.Tensor:
         # Tokenization and Inference
         torch.cuda.empty_cache()
         with torch.no_grad():
@@ -26,37 +25,24 @@ class ContrieverScorer:
                                     truncation=True, return_tensors='pt')
             for key in inputs:
                 inputs[key] = inputs[key].to(self.device)
-            outputs = self.query_encoder(**inputs)
-            # Mean Pool
-            token_embeddings = outputs[0]
-            mask = inputs["attention_mask"]
-            token_embeddings = token_embeddings.masked_fill(
-                ~mask[..., None].bool(), 0.)
-            sentence_embeddings = token_embeddings.sum(
-                dim=1) / mask.sum(dim=1)[..., None]
-            return sentence_embeddings
 
-    def get_embeddings(self, sentences: List[str]) -> torch.Tensor:
-        # Tokenization and Inference
-        torch.cuda.empty_cache()
-        with torch.no_grad():
-            inputs = self.tokenizer(sentences, padding=True,
-                                    truncation=True, return_tensors='pt')
-            for key in inputs:
-                inputs[key] = inputs[key].to(self.device)
-            outputs = self.reference_encoder(**inputs)
+            if mode == "query":
+                outputs = self.query_encoder(**inputs)
+            elif mode == "reference":
+                outputs = self.reference_encoder(**inputs)
+            else:
+                raise NotImplementedError
+
             # Mean Pool
             token_embeddings = outputs[0]
             mask = inputs["attention_mask"]
-            token_embeddings = token_embeddings.masked_fill(
-                ~mask[..., None].bool(), 0.)
-            sentence_embeddings = token_embeddings.sum(
-                dim=1) / mask.sum(dim=1)[..., None]
+            token_embeddings = token_embeddings.masked_fill(~mask[..., None].bool(), 0.)
+            sentence_embeddings = token_embeddings.sum(dim=1) / mask.sum(dim=1)[..., None]
             return sentence_embeddings
 
     def score_documents_on_query(self, query: str, documents: List[str]) -> torch.Tensor:
-        query_embedding = self.get_query_embeddings([query])[0]
-        document_embeddings = self.get_embeddings(documents)
+        query_embedding = self.get_embeddings([query], "query")[0]
+        document_embeddings = self.get_embeddings(documents, "reference")
         return query_embedding@document_embeddings.t()
 
     def select_topk(self, query: str, documents: List[str], k=1) -> torch.Tensor:
--- src/retriever/base_retriever.py
+++ src/retriever/base_retriever.py
@@ -2,6 +2,8 @@ from .playwright_based_crawl_new import FetchRawPage
 from ..model.contriver import ContrieverScorer
 import re
 from html2text import html2text
+from abc import ABC, abstractmethod
+from collections import defaultdict
 from typing import List, Dict, Tuple, Optional
 import json
 import asyncio
@@ -22,13 +24,14 @@ class SearchResult:
         return json.dumps(self.dump())
 
 
-class BaseRetriever:
+class BaseRetriever(ABC):
     def __init__(self, tokenizer_path, retriever_ckpt_path, device=None, scorer_max_batch_size=400) -> None:
         self.loop = asyncio.get_event_loop()
         self.scorer = ContrieverScorer(tokenizer_path, retriever_ckpt_path, device, scorer_max_batch_size)
 
+    @abstractmethod
     def get_search_result(self, question: str) -> List[SearchResult]:
-        raise NotImplementedError
+        pass
 
     def _search(self, question:str, max_tries:int=3) -> List[SearchResult]:
         cnt = 0
@@ -40,26 +43,27 @@ class BaseRetriever:
         print('No Result')
         return []
 
-    def _pre_handle_urls(self, urls: List[str]) -> List[str]:
-        urls_new = []
-        for url in urls:
-            if url in urls_new or "http://%s"%url in urls_new or "https://%s"%url in urls_new:
+    def _pre_handle_search_result(self, results: List[SearchResult]) -> defaultdict[str, str]:
+        url2title = defaultdict(str)
+        for result in results:
+            url = re.sub(r'#.*$', '', result.url)
+            if url in url2title.keys() or "http://%s"%url in url2title.keys() or "https://%s"%url in url2title.keys():
                 continue
             if not url.startswith("http"):
                 url = "http://%s" % url
-            urls_new.append(url)
-        return urls_new
+            url2title[url] = result.title
+        return url2title
 
     def _fetch(self, urls: List[str]) -> Dict[str, str]:
-        urls = self._pre_handle_urls(urls)
         self.loop.run_until_complete(FetchRawPage.get_raw_pages(urls, close_browser=True))
         return {url:text for url,text in FetchRawPage.results.items() if text is not None}
 
-    def _limit_length(self, paragraphs, low=50, high=1200):
+    def _limit_length(self, paragraphs, low, high):
         ret = []
         for item in paragraphs:
             item = item.strip()
             item = re.sub(r"\[\d+\]", "", item)
+            item = re.sub(r'\[([^\]]*)\]\(.*?\)', r'\1', item)
             if len(item) < low:
                 continue
             if len(item) > high:
@@ -67,30 +71,26 @@ class BaseRetriever:
             ret.append(item)
         return ret
 
-    def query(self, question:str):
+    def query(self, question:str, result_length_low:int=50, result_length_high:int=1200, topk:int=5):
         search_results = self._search(question)
         if len(search_results) == 0:
             return None
-        urls = []
-        titles = dict()
-        for result in search_results:
-            urls.append(result.url)
-            titles[result.url] = result.title
+        url2title = self._pre_handle_search_result(search_results)
 
-        fetch_results = self._fetch(urls)
+        fetch_results = self._fetch(url2title.keys())
         if len(fetch_results) == 0:
             return None
 
         data_list = []
         for url in fetch_results.keys():
-            extract_results = self._limit_length(html2text(fetch_results[url]).split("\n"))
+            extract_results = self._limit_length(html2text(fetch_results[url]).split("\n"), result_length_low, result_length_high)
             for value in extract_results:
                 data_list.append({
                     "url": url,
-                    "title": titles[url],
+                    "title": url2title[url],
                     "text": value
                 })
         if len(data_list) == 0:
             return None
 
-        return self.scorer(question, data_list, 5)
+        return self.scorer(question, data_list, topk)
