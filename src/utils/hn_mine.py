5dd2995ee415fc1cc0d09541d19fbc8079d39e07
温清梦 <flidwala@gmail.com>
2024-08-09 14:48:11 +0800

    负样本挖掘适配

--- src/utils/hn_mine.py
+++ src/utils/hn_mine.py
@@ -3,21 +3,23 @@ import json
 import random
 import numpy as np
 import faiss
+import torch
 from tqdm import tqdm
-from ..model.bgem3 import M3ForInference
-from arguments import ModelArguments
+from src.model.bgem3 import M3ForInference
+from src.utils.arguments import ModelArguments
+from typing import Dict, List
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', default="./data/drugs_dataset.json", type=str)
-    parser.add_argument('--output_file', default="./data/drugs_dataset_mine.json", type=str)
+    parser.add_argument('--input_file', default="./data/drugs_filtered.json", type=str)
+    parser.add_argument('--output_file', default="./data/drugs_neg.json", type=str)
     parser.add_argument('--range_for_sampling', default="10-210", type=str, help="range to sample negatives")
     parser.add_argument('--use_gpu_for_embedding', default=True, help='load model in gpu')
     parser.add_argument('--use_gpu_for_searching', default=False,
                         help='use faiss-gpu, faiss-gpu can only import on linux else faiss-cpu on win')
     parser.add_argument('--negative_number', default=15, type=int, help='the number of negatives')
-    parser.add_argument('--embed_batch_size', default=4,
+    parser.add_argument('--embed_batch_size', default=1,
                         help="batch size when getting query/corpus embedding, independent with search_batch_size")
     parser.add_argument('--search_batch_size', default=64, help="search batch size with baiss knn")
 
@@ -49,6 +51,7 @@ def batch_search(index: faiss.IndexFlatIP,
     return all_scores, all_inxs
 
 
+# TODO query和passage分开取负样本
 def find_knn_neg(
     model,
     input_file,
@@ -59,61 +62,50 @@ def find_knn_neg(
     embed_batch_size,
     search_batch_size,
 ):
-    '''
-    检索sample_range[-1]个最近的vec, 滤掉pos中的和与query相等的, 最终取negative_number个
-    '''
-    corpus = []
-    queries = []
-    train_data = []
-    for line in open(input_file):
-        line = json.loads(line.strip())
-        train_data.append(line)
-        corpus.extend(line['pos'])
-        if 'neg' in line:
-            corpus.extend(line['neg'])
-        queries.append(line['query'])
-    corpus = list(set(corpus))
 
-    p_vecs = model(corpus, batch_size=embed_batch_size)
-    q_vecs = model(queries, batch_size=embed_batch_size)
+    train_data:Dict[str, List[str]] = dict()
+    with open(input_file, "r") as f:
+        input_data:Dict[str, dict] = json.load(f)
+        drug_ids = list(input_data.keys())
+        corpus:List[str] = [d["description"] for d in input_data.values()]
+        # TODO random choice
+        queries:List[str] = [d["names"][0] for d in input_data.values()]
+
+    p_vecs = model(corpus, batch_size=embed_batch_size).detach().cpu()
+    q_vecs = model(queries, batch_size=embed_batch_size).detach().cpu()
 
     index = create_index(p_vecs, use_gpu=use_gpu)
     _, all_inxs = batch_search(index, q_vecs, topk=sample_range[-1], batch_size=search_batch_size)
-    assert len(all_inxs) == len(train_data)
+    assert len(all_inxs) == len(drug_ids)
 
-    for i, data in enumerate(train_data):
-        query = data['query']
+    for i in range(len(drug_ids)):
         inxs = all_inxs[i][sample_range[0]:sample_range[1]]
-        filtered_inx = []
-        for inx in inxs:
-            if inx == -1: break
-            if corpus[inx] not in data['pos'] and corpus[inx] != query:
-                filtered_inx.append(inx)
+        inxs = [inx for inx in inxs if inx != i]
 
-        if len(filtered_inx) > negative_number:
-            filtered_inx = random.sample(filtered_inx, negative_number)
-        data['neg'] = [corpus[inx] for inx in filtered_inx]
+        if len(inxs) > negative_number:
+            inxs = random.sample(inxs, negative_number)
+        elif len(inxs) < negative_number:
+            samples = random.sample(corpus, negative_number - len(inxs) + 1)
+            samples = [sent for sent in samples if sent != i]
+            inxs.extend(samples[: negative_number - len(inxs)])
+        train_data[drug_ids[i]] = [drug_ids[inx] for inx in inxs]
 
     with open(output_file, 'w') as f:
-        for data in train_data:
-            if len(data['neg']) < negative_number:
-                samples = random.sample(corpus, negative_number - len(data['neg']) + len(data['pos']))
-                samples = [sent for sent in samples if sent not in data['pos']]
-                data['neg'].extend(samples[: negative_number - len(data['neg'])])
-            f.write(json.dumps(data, ensure_ascii=False) + '\n')
+        f.write(json.dumps(train_data))
 
 
 if __name__ == '__main__':
-    args = get_args()
-    sample_range = list(map(int, args.range_for_sampling.split('-')))
-    model = M3ForInference(model_load_args=ModelArguments(), device="cuda" if args.use_gpu_for_embedding else "cpu")
+    with torch.no_grad():
+        args = get_args()
+        sample_range = list(map(int, args.range_for_sampling.split('-')))
+        model = M3ForInference(model_load_args=ModelArguments(), device="cuda" if args.use_gpu_for_embedding else "cpu")
 
-    find_knn_neg(model,
-                 input_file=args.input_file,
-                 output_file=args.output_file,
-                 sample_range=sample_range,
-                 negative_number=args.negative_number,
-                 use_gpu=args.use_gpu_for_searching,
-                 embed_batch_size=args.embed_batch_size,
-                 search_batch_size=args.search_batch_size
-                 )
+        find_knn_neg(model,
+                    input_file=args.input_file,
+                    output_file=args.output_file,
+                    sample_range=sample_range,
+                    negative_number=args.negative_number,
+                    use_gpu=args.use_gpu_for_searching,
+                    embed_batch_size=args.embed_batch_size,
+                    search_batch_size=args.search_batch_size
+                    )
