76ffdd0c84bacee09a452910d2b16b2273b67d52
温清梦 <flidwala@gmail.com>
2025-01-17 14:20:53 +0800

    优化retriever，似乎被封ip，待改用其他框架

--- src/retriever/bing_retriever.py
+++ src/retriever/bing_retriever.py
@@ -16,57 +16,49 @@ class BingRetriever(BaseRetriever):
         elif isinstance(question, list):
             return {'q':f'icd+10+{question[0]}+{question[1].replace(" ", "+")}+Symptoms', 'first':first, 'FORM':'Z9FD1'}
 
+    def _sleep(self):
+        time.sleep(random.random()*2)
+
+    def _search_bing(self, question: Union[str, List[str]], first:int=1):
+        while True:
+            self._sleep()
+            try:
+                response = requests.get(
+                    "https://cn.bing.com/search",
+                    params=self._kw(question, first),
+                    headers=self.headers,
+                    timeout=20,
+                )
+                if response.status_code != 200:
+                    continue
+                response = bs(response.text, "html.parser")
+                results = map(
+                    lambda result: result.find("a", _ctf="rdr_T"),
+                    response.find('ol', id="b_results").find_all("div", class_="b_algoheader")
+                )
+                results = [SearchResult(result.find('h2').get_text(), result.attrs['href']) for result in results]
+                break
+            except:
+                continue
+        return results
+
     def get_search_result(
         self,
         question: Union[str, List[str]],
         min_search_result_num: Optional[int] = None,
     ) -> List[SearchResult]:
         if min_search_result_num is None:
-            while True:
-                try:
-                    response = requests.get(
-                        "https://cn.bing.com/search",
-                        params=self._kw(question),
-                        headers=self.headers,
-                        timeout=20
-                    )
-                    break
-                except:
-                    time.sleep(random.randint(1, 8))
-            response = bs(response.text, "html.parser")
-            results = map(
-                lambda result: result.find("a", _ctf="rdr_T"),
-                response.find('ol', id="b_results").find_all("div", class_="b_algoheader")
-            )
-            results = [SearchResult(result.find('h2').get_text(), result.attrs['href']) for result in results]
+            results = self._search_bing(question)
             return results
         else:
             results = []
             first = 1
             while len(results) < min_search_result_num:
-                while True:
-                    try:
-                        response = requests.get(
-                            "https://cn.bing.com/search",
-                            params=self._kw(question),
-                            headers=self.headers,
-                            timeout=20
-                        )
-                        if response.status_code != 200:
-                            continue
-                        break
-                    except:
-                        time.sleep(random.randint(1, 8))
-                response = bs(response.text, "html.parser")
-                temp = map(
-                    lambda result: result.find("a", _ctf="rdr_T"),
-                    response.find('ol', id="b_results").find_all("div", class_="b_algoheader")
-                )
-                temp = [SearchResult(result.find('h2').get_text(), result.attrs['href']) for result in temp]
+                self._sleep()
+                temp = self._search_bing(question, first)
                 if len(temp) == 0:
-                    break
+                    continue
                 else:
                     results.extend(temp)
                 first += 10
-                time.sleep((random.random()+0.5)*2)
             return results
